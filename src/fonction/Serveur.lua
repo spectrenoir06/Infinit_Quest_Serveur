8000ca0247b04a2f0f00aea35523b6d9c222bde0
spectrenoir06 <antoine@doussaud.org>
2014-05-30 01:18:56 +0200

    add tab clients = { udp , tcp }

--- src/fonction/Serveur.lua
+++ src/fonction/Serveur.lua
@@ -19,11 +19,11 @@ function Server.new(ip,port,sync_dt)
 
 
 	a.save 	= {}  		--
-	a.peer 	= {}       	-- table avec les clients
+	a.clients 	= {}       	-- table avec les clients
 	a.perso	= {}   		-- table avec les perso des clients
 
 	for i=1,10 do
-		a.peer[i]	= {}   	-- creation des salons peer
+		a.clients[i]	= {}   	-- creation des salons client
 		a.perso[i]	= {}  	-- creation des salons perso
 	end
 	return a
@@ -32,41 +32,31 @@ end
 
 function Server:update()
 
-	self.sync = socket.gettime() -- temp du debut de la frame
 	local data, msg_or_ip, port_or_nil = self.serverUdp:receivefrom()
 
 	if data then
-		print(data, msg_or_ip, port_or_nil)
+		--print(data, msg_or_ip, port_or_nil)
 		self:receive(data,msg_or_ip..':'..port_or_nil)
 	end
-	
-
-	-- if self.compteur > self.sync_dt then
-		-- self:send_update()
-		-- self.compteur = 0
-	-- end
-
-	-- self.dt = socket.gettime() - self.sync 	-- temp de la frame
-	-- self.compteur = self.compteur + self.dt -- addition du temp de la frame
 
 end
 
-function Server:receive(data,peer)
+function Server:receive(data,client)
     --print(data,peer)
     --print("receive : port="..port.." ; cmd="..json.decode(data).cmd)
     
     --local tab = json.decode(data)
     
-	if pcall(function() local tab = json.decode(data) end) then
+	if false then --pcall(function() local tab = json.decode(data) end) then
 		--print("json conforme")
 		if tab.cmd == "login" then
-			self:login(tab.data,peer)
+			self:login(tab.data,client)
 		elseif tab.cmd == "pos_update" then
-			self:pos_update(tab.data,peer)
+			self:pos_update(tab.data,client)
 		elseif tab.cmd == "change_map" then
-			self:change_map(tab.data,peer)
+			self:change_map(client)
 		else
-			print("cmd inconnu : "..tab.cmd,peer)
+			print("cmd inconnu : "..tab.cmd,client)
 		end
     else
       print("Error pas de json ", data)
@@ -75,22 +65,22 @@ function Server:receive(data,peer)
 
 end
 
-function Server:login(data,peer)
-    print("add player",data.name,peer)
+function Server:login(data,client)
+    print("add player",data.name)
     local newplayer = player_new(data) -- creation du nouveau joueur coter Server
-    self:join_map(1,newplayer,peer)
+    self:join_map(1,newplayer,client)
 end
 
 function Server:exit_map(map,nb)
     print("player "..self.perso[map][nb].name.." exit map "..map)
     table.remove(self.perso[map],nb)
-    table.remove(self.peer[map],nb)
+    table.remove(self.clients[map],nb)
     self:broadcast_all_map("player_exit_map",{nb = nb},map)
 
     --print(#self.peer[map].." joueur(s) restant")
 end
 
-function Server:join_map(map,player,peer)
+function Server:join_map(map,player,client)
     --print(json.encode(player))
     print("player "..player.name.." join map "..map)
     
@@ -98,17 +88,17 @@ function Server:join_map(map,player,peer)
     
     local tab = { players = self.perso[map], }
     --print(peer)
-    self:send("join_map",tab,peer)
+    self:send("join_map",tab,skt)
     self:broadcast_all_map("player_join_map",player:getinfo(),map) -- envoi a tout le monde les info sur le nouveau joueur
     
-    table.insert(self.peer[map],peer)             --  ajout du nouveau peer du nouveau joueur a la liste
+    table.insert(self.clients[map],client)             --  ajout du nouveau peer du nouveau joueur a la liste
 end
 
-function Server:change_map(data,peer)
+function Server:change_map(data,client)
 
-  local old_map,old_nb = self:getMapNb(peer) -- recupertation ancienne map et nb
+  local old_map,old_nb = self:getMapNb(client) -- recupertation ancienne map et nb
   --print(json.encode(data))
-  self:pos_update(data,peer)
+  self:pos_update(data,client)
   
   --local map,nb = self:getMapNb(peer) -- recuperation nouvelle map et nb
   
@@ -117,12 +107,12 @@ function Server:change_map(data,peer)
   table.insert(self.perso[data.map],self.perso[old_map][old_nb]) -- copie du perso dans nouvelle map
   
   --print(json.encode(self.perso[data.map]))
-  self:send("join_map",{players=self.perso[data.map]},peer) -- envoit les players de la map d'arriver au client
+  self:send("join_map",{players=self.perso[data.map]},client) -- envoit les players de la map d'arriver au client
   self:broadcast_all_map("player_join_map",self.perso[data.map][#self.perso[data.map]]:getinfo(),data.map) -- previent tout les client de la nouvelle map de l'arriver du nouveau
   
-  table.insert(self.peer[data.map],peer) -- rajout du peer du nouveau dans la map d'arriver
+  table.insert(self.clients[data.map],client) -- rajout du peer du nouveau dans la map d'arriver
   table.remove(self.perso[old_map],old_nb) -- supression du perso de l'anciene map
-  table.remove(self.peer[old_map],old_nb)  -- supression du peer de l'ancienne map
+  table.remove(self.clients[old_map],old_nb)  -- supression du peer de l'ancienne map
   self:broadcast_all_map("player_exit_map",{nb = old_nb},old_map) -- informe les clients du depart de leur pote  
   
   for i=1,3 do
@@ -130,18 +120,18 @@ function Server:change_map(data,peer)
   end
 end
 
-function Server:disconnect(peer)
-  local map,nb = self:getMapNb(peer)
-  print(peer,"disconnect map "..map.." joueur number "..nb)
+function Server:disconnect(client)
+  local map,nb = self:getMapNb(client)
+  print(client,"disconnect map "..map.." joueur number "..nb)
   self:exit_map(map,nb)
 end
 
-function Server:send(cmd,data,peer)
-    peer:send(json.encode({cmd = cmd , data = data}))
+function Server:send(cmd,data,client)
+    self.serverUdp:sendto(json.encode({cmd = cmd , data = data}),client.ip,client.port)
 end
 
 function Server:broadcast_all_map(cmd,data,map)
-    for k,v in ipairs(self.peer[map]) do
+    for k,v in ipairs(self.clients[map]) do
         self:send(cmd,data,v)
     end
 end
@@ -152,18 +142,18 @@ function Server:send_update()
   end
 end
 
-function Server:getNb(peer,map)
-  for k,v in ipairs(self.peer[map]) do
-    if peer==v then
+function Server:getNb(client,map)
+  for k,v in ipairs(self.clients[map]) do
+    if (client.ip == v.ip) and (client.port == v.port) then
       return k
     end 
   end
   return false
 end
 
-function Server:getMapNb(peer)
-  for k,v in ipairs(self.peer) do
-    local n = self:getNb(peer,k)
+function Server:getMapNb(client)
+  for k,v in ipairs(self.clients) do
+    local n = self:getNb(client,k)
     if n then
       return k,n -- k==map , n==index
     end 
@@ -171,8 +161,8 @@ function Server:getMapNb(peer)
   return false
 end
 
-function Server:pos_update(data,peer)
-  local map , nb = self:getMapNb(peer)
+function Server:pos_update(data,client)
+  local map , nb = self:getMapNb(client)
   self.perso[map][nb]:setinfo(data)  
 end
 ------------------------------------------------------------------------------------
--- src/start.lua
+++ src/start.lua
@@ -20,22 +20,31 @@ local server = assert(socket.bind("*", 100))
 i	= 0
 nb	= 0
 cl 	= 0
+
+Clients = {}
 function handler(skt)
-	print(skt)
+	
 	skt = copas.wrap(skt)
+	local tcpIp, tcpPort = skt.socket:getpeername()
+	local udpClient = skt:receive()
+	local udpIp, udpPort = string.match(udpClient, '(.*):(%d*)')
+
+	print("client nb "..cl..", TPC = "..tcpIp..':'..tcpPort.." ,  UDP = "..udpClient)
+	
+	table.insert(Clients,{tcp = { ip = tcpIp, port = tcpPort }, udp = { ip = udpIp, port = udpPort }})
+	
+	skt:send("Bienvenue client nb "..cl..", TPC = "..tcpIp..':'..tcpPort.." ,  UDP = "..udpClient.."\n")
+	
 	cl = cl +1
 	while true do
 		nb=nb+1
 		local data = skt:receive()
-		print(data)
-		if data == "time" then
-			print"Zboob"
-			skt:send("Zboob\n")
-		elseif data then
+		--print(data)
+		if data then
 			local tab = json.decode(data)
 			--print(tab.cmd)
 			if tab.cmd == "login" then
-				--self:login(tab.data,skt)
+				self:login(tab.data,skt)
 			elseif tab.cmd == "change_map" then
 				--self:change_map(tab.data,skt)
 			else
@@ -55,9 +64,11 @@ copas.addserver(server, handler)
 while 1 do
     --server:update()
 	copas.step(0) -- rajoute client
-	print("boucle : "..i..", client : "..cl..", handler : "..nb)
+	
+	print("\nboucle : "..i..", client : "..cl..", handler : "..nb)
+	for k,v in ipairs(Clients) do print(k.." tcp : "..v.tcp.ip..":"..v.tcp.port.." udp : "..v.udp.ip..":"..v.udp.port) end
 	udpTest:update()
-	socket.sleep(0.5)
+	socket.sleep(0.25)
 	i=i+1
 end
 
